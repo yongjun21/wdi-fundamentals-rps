f5f366773ac6c72ed34ce3cfc9d84469591ba1f4
Yong Jun <yjthong.2009@smu.edu.sg>
2015-11-03 09:39:01 +0800

    Improved

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -4,6 +4,7 @@
 'use strict';
 
 function getInput() {
+    console.log(' ')
     console.log("Please choose either 'rock', 'paper', or 'scissors'.");
     return prompt();
 }
@@ -42,41 +43,41 @@ function getWinner(playerMove,computerMove) {
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
-	var p;
-	var c;    
-	switch(playerMove) {
-		case "rock":
-			p=1;
-			break;
-		case "paper":
-			p=2;
-			break;
-		case "scissors":
-			p=3;
-			break;
-	}
-	switch(computerMove) {
-		case "rock":
-			c=4;
-			break;
-		case "paper":
-			c=5;
-			break;
-		case "scissors":
-			c=6;
-			break;
-	}
-	if(!(p && c)) {winner="tie";}
-	switch((c-p)%3) {
-		case 1:
-			winner="computer";
-			break;
-		case 2:
-			winner="player";
-			break;
-		default:
-			winner="tie";
-	}
+    var p;
+    var c;
+    switch(playerMove) {
+        case "rock":
+            p=1;
+            break;
+        case "paper":
+            p=2;
+            break;
+        case "scissors":
+            p=3;
+            break;
+    }
+    switch(computerMove) {
+        case "rock":
+            c=4;
+            break;
+        case "paper":
+            c=5;
+            break;
+        case "scissors":
+            c=6;
+            break;
+    }
+    if(!(p && c)) {return "invalid";}
+    switch((c-p)%3) {
+        case 1:
+            winner="computer";
+            break;
+        case 2:
+            winner="player";
+            break;
+        default:
+            winner="tie";
+    }
     return winner;
 }
 
@@ -90,16 +91,19 @@ function playToFive() {
     while(playerWins<5 && computerWins<5) {
         playerMove=getPlayerMove();
         computerMove=getComputerMove();
-		switch(getWinner(playerMove,computerMove)) {
-			case "player":
-				playerWins++;
-				break;
-			case "computer":
-				computerWins++;
-				break;
-		}
-		console.log('Player chose ' + playerMove + ' while Computer chose ' + computerMove);
-        console.log('The score is currently ' + playerWins + ' to ' + computerWins + '\n');
-	}
-	return [playerWins, computerWins];
+        switch(getWinner(playerMove,computerMove)) {
+            case "player":
+                playerWins++;
+                break;
+            case "computer":
+                computerWins++;
+                break;
+            case "invalid":
+                console.log('Invalid move, try again.');
+                continue;
+        }
+        console.log('Player chose ' + playerMove + ' while Computer chose ' + computerMove);
+        console.log('The score is currently ' + playerWins + ' to ' + computerWins);
+    }
+    return [playerWins, computerWins];
 }
